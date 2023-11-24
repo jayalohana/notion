0498a63b1bd64b036f7d80a9dc57c55eedefa8c3
Jaya <jaya.lohana24@gmail.com>
2023-11-24 12:15:20 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="text-white bg-emerald-500">Delete</Button>
+    </div>
+  )
+}
